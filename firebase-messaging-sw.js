6742a46bbc807ef3a517cf54663ac894b42d4b01
networknetty <58984350+networknetty@users.noreply.github.com>
2019-12-17 17:03:10 +0200

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,49 @@
+
+
+importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js');
+
+importScripts('./libs/localforage.min.js');
+
+function getLocalData(key, callback){
+    localforage.getItem(key).then(function(value) {
+        callback(value);
+    }).catch(function(err) {
+        callback();
+    });
+}
+
+let _configkey = "config";
+
+getLocalData(_configkey, back);
+
+function back(cfg){
+
+    if(cfg != null){
+
+        let firebaseConfig2 = {
+            apiKey: cfg.apiKey,
+            projectId: cfg.projectId,
+            messagingSenderId: cfg.messagingSenderId,
+            appId: cfg.appId
+        };
+
+        firebase.initializeApp(firebaseConfig2);
+
+        const messaging = firebase.messaging();
+
+        messaging.setBackgroundMessageHandler(function(payload) {
+            console.log('[firebase-messaging-sw.js] Received background message ', payload);
+            const notificationTitle = 'Background Message Title';
+            const notificationOptions = {
+                body: 'Background Message body.',
+                icon: '/firebase-logo.png'
+            };
+
+            return self.registration.showNotification(notificationTitle,
+                notificationOptions);
+
+        });
+    }
+}
+
---
+++ main.js
@@ -0,0 +1,196 @@
+
+
+window.onload = function () {
+
+    if (!window.localStorage) {
+        console.log("!window.localStorage");
+    }
+
+    let _configkey = "config";
+
+    let setLocalObject = function(key, value) {
+        window.localStorage.setItem(key, JSON.stringify(value));
+
+        localforage.setItem(key, value)
+            .then(function (value) {})
+            .catch(function (error) {
+                console.log('localforage.setItem err: '+error);
+            });
+    };
+
+    let getLocalObject = function(key) {
+        if(!window.localStorage.hasOwnProperty(key))
+            return null;
+        return JSON.parse(window.localStorage.getItem(key));
+    };
+
+    let _config = getLocalObject(_configkey);
+    if(_config == null){
+        _config = { url:"http://localhost:3000", jwt:"", apiKey: "", authDomain: "", databaseURL: "",
+            projectId: "",  storageBucket: "", messagingSenderId: "", appId: "", measurementId: "" };
+        setLocalObject(_configkey, _config);
+        onConfigEdit();
+    }else{
+        $('#form_div').addClass('hide_view');
+        $('#info_config').html('url: '+_config.url+' <br> ' +
+            'apiKey: '+_config.apiKey +' <br> ' +
+            'authDomain: '+_config.authDomain +' <br> ' +
+            'databaseURL: '+_config.databaseURL +' <br> ' +
+            'projectId: '+_config.projectId +' <br> ' +
+            'storageBucket: '+_config.storageBucket +' <br> ' +
+            'messagingSenderId: '+_config.messagingSenderId +' <br> ' +
+            'appId: '+_config.appId +' <br> ' +
+            'measurementId: '+_config.measurementId
+        );
+    }
+
+
+    let info_t_fb = document.getElementById('info_t_fb');
+    let info_t_fcm = document.getElementById('info_t_fcm');
+    let info_res = document.getElementById('info_res');
+    let info_msg = document.getElementById('info_msg');
+
+    let tk_fb;
+    let tk_fcm;
+
+    $('.btn_save').on('click', onConfigSave);
+
+    function onConfigSave() {
+
+        let formdt = new FormData(  document.getElementById("userForm")  );
+
+        _config.url = formdt.get('url');
+        _config.jwt = formdt.get('jwt');
+        _config.apiKey = formdt.get('apiKey');
+        _config.authDomain = formdt.get('authDomain');
+        _config.databaseURL = formdt.get('databaseURL');
+        _config.projectId = formdt.get('projectId');
+        _config.storageBucket = formdt.get('storageBucket');
+        _config.messagingSenderId = formdt.get('messagingSenderId');
+        _config.appId = formdt.get('appId');
+        _config.measurementId = formdt.get('measurementId');
+
+        setLocalObject(_configkey, _config);
+
+        $('#form_div').addClass('hide_view');
+        $('#form_info').removeClass('hide_view');
+
+        $('#info_config').html('url: '+_config.url+' <br> ' +
+            'apiKey: '+_config.apiKey +' <br> ' +
+            'authDomain: '+_config.authDomain +' <br> ' +
+            'databaseURL: '+_config.databaseURL +' <br> ' +
+            'projectId: '+_config.projectId +' <br> ' +
+            'storageBucket: '+_config.storageBucket +' <br> ' +
+            'messagingSenderId: '+_config.messagingSenderId +' <br> ' +
+            'appId: '+_config.appId +' <br> ' +
+            'measurementId: '+_config.measurementId
+        );
+    }
+
+    $('.btn_send').on('click', onSend);
+
+    function onSend() {
+
+        $('.buttons').addClass('hide_view');
+
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('firebase-messaging-sw.js').then(function(registration) {
+            }).catch(function(error) {
+                console.log('error reg service worker:', error);
+            });
+        } else {
+            console.log('browser service worker not sup');
+        }
+
+        firebase.initializeApp(
+            {
+                apiKey: _config.apiKey,
+                authDomain: _config.authDomain,
+                databaseURL: _config.databaseURL,
+                projectId: _config.projectId,
+                storageBucket: _config.storageBucket,
+                messagingSenderId: _config.messagingSenderId,
+                appId: _config.appId,
+                measurementId: _config.measurementId
+            }
+        );
+
+        let messaging = firebase.messaging();
+
+        messaging.requestPermission()
+            .then(
+                function () {
+                    console.log("Notification permission granted");
+                    return messaging.getToken();
+                }
+            )
+            .catch( function (err) { console.log("Unable to get permission to notify.", err); } );
+
+        messaging.onMessage((payload) => {
+            let msg = JSON.stringify(payload);
+            info_msg.innerHTML = 'Message received: '+ msg;
+            new Notification(payload.notification.title, payload.notification);
+        });
+
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            var user = result.user;
+            tk_fb = user.ma.toString();
+            info_t_fb.innerHTML = "token firebase:   "+tk_fb;
+            messaging.getToken()
+                .then(
+                    (tok)=>{
+                        tk_fcm = tok;
+                        info_t_fcm.innerHTML = 'token fcm:   '+tk_fcm;
+                        callBackend();
+                    }
+                );
+        }).catch(function(error) {console.log('error: ', error);});
+    }
+
+    $('.btn_edit').on('click', onConfigEdit);
+
+    function onConfigEdit() {
+        $("#cfg_url").val(_config.url);
+        $("#cfg_jwt").val(_config.jwt);
+        $("#cfg_apiKey").val(_config.apiKey);
+        $("#cfg_authDomain").val(_config.authDomain);
+        $("#cfg_databaseURL").val(_config.databaseURL);
+        $("#cfg_projectId").val(_config.projectId);
+        $("#cfg_storageBucket").val(_config.storageBucket);
+        $("#cfg_messagingSenderId").val(_config.messagingSenderId);
+        $("#cfg_appId").val(_config.appId);
+        $("#cfg_measurementId").val(_config.measurementId);
+
+        $('#form_div').removeClass('hide_view');
+        $('#form_info').addClass('hide_view');
+    }
+
+    let callBackend = function (){
+        let body = { data:{token_fire:tk_fb, fcm:tk_fcm}, action:'login_fire_base_token' };
+        fetch(
+            _config.url+'/login',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type' : 'application/json',
+                        'jwt': _config.jwt
+                    },
+                    body: JSON.stringify(body)
+                }
+            )
+            .then( function (response) {
+                    if (response.ok) {
+                        response.json()
+                            .then( function (body) {
+                                let bodyStr = JSON.stringify(body);
+                                info_res.innerHTML = "response.body:   "+bodyStr;
+                            });
+                    } else { alert("error HTTP: " + response.status);}
+                }
+            );
+    };
+
+
+};
