c67a85cedeb6338305e522cbbddced51cdc1c725
krowtenytten <krowtenytten@gmail.com>
2020-05-02 16:29:20 +0300

    no message

--- js/notification.js
+++ js/notification.js
@@ -76,32 +76,41 @@ function create_notification(context) {
 
     let _createCloseNotification = function (trigger, data) {
         let ix;
+        let div;
+
         function close(){
             let dv = document.getElementById("notification_block_"+ix);
             $('#notification_block').fadeOut(2000);
             setTimeout(dv.remove, 2000);
         }
+
         if(trigger[0] === "call"){
             // $('#notification_block').fadeIn();
             ix = Math.random().toString(36).substring(2, 15) +
                 Math.random().toString(36).substring(2, 15);
 
-            _div.appendChild("<div class='notification_block' id='notification_block_"+ix+"'>"+
-                "<div class='notification_text'>"+data.notification+"</div>"+
+            div = document.createElement('div');
+            div.className = 'notification_block';
+            div.id = 'notification_block_'+ix;
+            _div.appendChild(div);
+
+            div.innerHTML = "<div class='notification_text'>"+data.notification+"</div>"+
                 "<div class='notification_text_info'>"+JSON.stringify(data)+"</div>" +
-                "<input type='button' value='x' class='buttons' id='btn_ntfc_close_"+ix+"'>" +
-                "</div>");
+                "<input type='button' value='x' class='buttons' id='btn_ntfc_close_"+ix+"'>";
 
             $('#btn_ntfc_close_'+ix).on('click', close);
         }
-
         else if(trigger[0] === "call_trigger_message"){
             ix = Math.random().toString(36).substring(2, 15) +
                 Math.random().toString(36).substring(2, 15);
 
-            _div.appendChild("<div class='notification_block' id='notification_block'>"+
-                "<div class='notification_text'>"+trigger[1]+"</div>"+
-                "<div class='notification_text_info'>"+JSON.stringify(data)+"</div></div>");
+            div = document.createElement('div');
+            div.className = 'notification_block';
+            div.id = 'notification_block_'+ix;
+            _div.appendChild(div);
+
+            div.innerHTML = "<div class='notification_text'>"+trigger[1]+"</div>"+
+                "<div class='notification_text_info'>"+JSON.stringify(data)+"</div>";
 
             $('#btn_ntfc_close_'+ix).on('click', close);
         }
