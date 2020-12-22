21272c8f48e7a7b5a74cc5b771343fe00942943b
avocodeuser000 <avocodeuser000@gmail.com>
2020-12-22 13:14:42 +0200

    no message

--- js/parse_logs.js
+++ js/parse_logs.js
@@ -6,7 +6,12 @@ function logsComponentLoginDone(context) {
     document.getElementById("info_buttons_block").innerHTML = "<input type='button' value='LOGS' id='btn_open_logs_screen'>";
     $('#btn_open_logs_screen').on('click', openLogsScreen);
     $('#btn_close_logs_screen').on('click', closeLogsScreen);
-    $('#btn_get_all_logs').on('click', fullLogsUpdate);
+    $('#btn_get_all_logs').on('click', () => {
+        _context.callRest('/admin', {data:{}, action:'get_logs_list'}, data => {
+            // console.log('logs component [get logs list] data: '+JSON.parse(data));
+            updateLogsList(data.data);
+        })
+    });
 }
 
 function openLogsScreen() {
@@ -21,11 +26,56 @@ function closeLogsScreen() {
 
 }
 
-function fullLogsUpdate() {
+let _logs_area = document.getElementById("logs_work_area");
+function createLogsBlock(name) {
 
-    _context.callRest('/admin', {data:{}, action:'get_logs_list'}, data => {
-        console.log('logs component [get logs list] data: '+data);
-    })
+    function createButtonToHead(btn_name, parent) {
+        let b_up = document.createElement('input');
+        b_up.type = 'button';
+        b_up.value = btn_name;
+        b_up.class = 'buttons';
+        b_up.id = 'btn_'+btn_name+'_'+name;
+        parent.appendChild(b_up);
+    }
+
+    let e = document.createElement('div');
+    e.class = 'logs_block';
+    _logs_area.appendChild(e);
+
+    let h = document.createElement('div');
+    h.class = '';
+    e.appendChild(h);
+
+    let t = document.createElement('div');
+    t.class = 'logs_block_title';
+    t.innerHTML = name;
+    h.appendChild(t);
+    // e.innerHTML = '<span class="logs_block_title">'+name+'</span>';
+
+    let bb = document.createElement('div');
+    bb.class = 'logs_block_buttons';
+    h.appendChild(bb);
+
+    createButtonToHead('update', bb);
+    createButtonToHead('open', bb);
+    createButtonToHead('close', bb);
+
+    let c = document.createElement('div');
+    c.class = 'logs_block_content';
+    c.id = 'content_'+name;
+    e.appendChild(c);
+}
+
+function updateLogsList(data) {
+
+    for(let i=0; i<data.length; i++)
+        createLogsBlock(data[i]);
+
+}
+
+function updateFullLogs(data) {
+
+
 
 }
 
